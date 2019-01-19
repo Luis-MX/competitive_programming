4c4f76a02d63aff5f9e4346ed8409708cd740428
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2019-01-18 22:16:06 -0600

    Organizando

---
+++ UVa/compet_programming_3/capitulo-01/p00230.cpp
@@ -0,0 +1,12 @@
+// Borrowers (Prestatarios)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	/* freopen("../in.txt", "r", stdin); */
+	/* freopen("../out.txt", "w", stdout); */
+	return 0;
+}
